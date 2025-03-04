e6d8fcc10599c69acb0c7a011fcf51fc72ff8613
nimkoes <xxxelppa@gmail.com>
2025-03-05 00:33:35 +0900

    모바일 환경에서 확대 축소 막기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
     <html lang="ko">
     <head>
       <title>Nimkoes Tech Blog</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     </head>
     <body className={styles.home}>
     <NavigationView />
