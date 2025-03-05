725c66680a49d319f26ab0fcb7ffaec152bff1d6
nimkoes <xxxelppa@gmail.com>
2025-03-05 21:02:01 +0900

    광고 처리 확인중

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,30 @@
 import { MetadataRoute } from 'next'
 
 const DOMAIN = 'https://nimkoes.github.io'
+const REPO = 'tech-blog'
 
+// robots.txt 형식의 문자열 생성
+export function generateRobotsContent() {
+  return `User-agent: *
+Allow: /
+Allow: /tech-blog/
+Disallow: /private/
+Disallow: /admin/
+Disallow: /*?*
+Disallow: /*.json$
+
+User-agent: GPTBot
+Disallow: /
+
+User-agent: CCBot
+Disallow: /
+
+Sitemap: https://raw.githubusercontent.com/nimkoes/${REPO}/main/sitemap.xml
+Host: ${DOMAIN}
+`
+}
+
+// Next.js의 robots API를 위한 기본 export
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
@@ -24,7 +47,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: ['/'],
       },
     ],
-    sitemap: `${DOMAIN}/sitemap.xml`,
+    sitemap: `https://raw.githubusercontent.com/nimkoes/${REPO}/main/sitemap.xml`,
     host: DOMAIN,
   }
 } 
