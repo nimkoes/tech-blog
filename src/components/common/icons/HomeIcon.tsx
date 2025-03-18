59df9845673b13d6980dce2448ba345ee34ed93a
nimkoes <xxxelppa@gmail.com>
2025-03-18 23:31:05 +0900

    home 버튼 위치 변경

---
+++ src/components/common/icons/HomeIcon.tsx
@@ -0,0 +1,20 @@
+const HomeIcon = ({ width, height }: { width: number; height: number }) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M12 3L2 12h3v8h6v-6h2v6h6v-8h3L12 3z"
+      fill="#61dafb"
+      stroke="#20232a"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default HomeIcon; 
